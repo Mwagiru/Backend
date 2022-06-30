fcea7bf9b12f845a6840e8606339edab2890b1dc
Mwagiru <joymwagiru@gmail.com>
2022-06-30 15:18:02 +0300

    Create routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,32 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/quotes" do
+    quote=Quote.find(rand(1..Quote.all.size))
+    quote.to_json
   end
-
+  post "/quotes" do
+    newQuote=Quote.create(
+      author_id: params[:author],
+      content: params[:quote]
+    )
+    newQuote.to_json
+  end
+  get "/allquotes" do
+    quote=Quote.all
+    quote.to_json
+  end
+  patch "/quotes/:id" do
+    quote = Quote.find(params[:id])
+    quote.update(body: params[:body])
+    quote.to_json
+  end
+  delete "/quotes/:id" do
+    quote= Quote.find(params[:id])
+    quote.destroy
+    quote.to_json
+  end
 end
