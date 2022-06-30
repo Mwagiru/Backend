0f076e87443aabee938b86d8b4c92b2c7ed81bbf
Mwagiru <joymwagiru@gmail.com>
2022-06-30 14:41:08 +0300

    Create authors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_113609) do
+ActiveRecord::Schema.define(version: 2022_06_30_113923) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.text "content"
